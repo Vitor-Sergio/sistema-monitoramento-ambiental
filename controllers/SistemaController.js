7d27ddcade0d8c1a7b9cd9ab376e962b47f90f88
Vitor-Sergio <vitorsergio0072@gmail.com>
2026-06-20 16:53:35 -0300

    Organizando estrutura do projeto

--- controllers/SistemaController.js
+++ controllers/SistemaController.js
@@ -1,28 +1,81 @@
 class SistemaController {
   constructor() {
-    this.sensores = [];
-    this.leituras = [];
+    this.sensores = []
+    this.leituras = []
   }
 
   cadastrarSensor(sensor) {
-    this.sensores.push(sensor);
-    console.log("Sensor Registrado");
+    this.sensores.push(sensor)
+    console.log("Sensor Registrado")
   }
 
   registraleituras(leitura) {
-    this.leituras.push(leitura);
-    leitura.sensorresponsavel.registrarLeitura(leitura.valor);
+    this.leituras.push(leitura)
+    leitura.sensorresponsavel.registrarLeitura(leitura.valor)
+  }
+
+  listarTodosSensores() {
+    console.log("Todos os sensores:")
+
+    for (let i = 0; i < this.sensores.length; i++) {
+      let s = this.sensores[i]
+
+      console.log(
+        "ID:", s.identificador,
+        "|", s.tipo,
+        "|", s.regiao,
+        "| Status:", s.status,
+        "| Alertas:", s.alertas
+      )
+    }
+  }
+
+  listarLeituras() {
+    console.log("Leituras registradas:")
+
+    for (let i = 0; i < this.leituras.length; i++) {
+      let l = this.leituras[i]
+
+      console.log(
+        "Sensor:", l.sensorresponsavel.identificador,
+        "| Valor:", l.valor,
+        "| Data:", l.data,
+        "| Hora:", l.hora
+      )
+    }
   }
 
   listarSensoresAtivos() {
-    console.log("Sensores ativos:");
+    console.log("Sensores ativos:")
 
-    for (let s of this.sensores) {
+    for (let i = 0; i < this.sensores.length; i++) {
+      let s = this.sensores[i]
+
       if (s.status === "ligado") {
-        console.log(`ID: ${s.identificador} | ${s.tipo} | ${s.regiao}`);
+        console.log(
+          "ID:", s.identificador,
+          "|", s.tipo,
+          "|", s.regiao
+        )
+      }
+    }
+  }
+
+  listarSensoresManutencao() {
+    console.log("Sensores em manutenção:")
+
+    for (let i = 0; i < this.sensores.length; i++) {
+      let s = this.sensores[i]
+
+      if (s.status === "manutencao") {
+        console.log(
+          "ID:", s.identificador,
+          "|", s.tipo,
+          "|", s.regiao
+        )
       }
     }
   }
 }
 
-module.exports = SistemaController;
+module.exports = SistemaController
--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-const Sensor = require("./classes/Sensor");
-const Leitura = require("./classes/Leitura");
-const SistemaController = require("./controllers/SistemaController");
+const Sensor = require("./classes/Sensor")
+const Leitura = require("./classes/Leitura")
+const SistemaController = require("./controllers/SistemaController")
 
-const sistema = new SistemaController();
+const sistema = new SistemaController()
 
-const sensor1 = new Sensor("S01", "Temperatura", "Centro", "ligado");
+const sensor1 = new Sensor("S001", "Temperatura", "Centro", "ligado")
+const sensor2 = new Sensor("S002", "Umidade", "Norte", "ligado")
 
-sistema.cadastrarSensor(sensor1);
+sistema.cadastrarSensor(sensor1)
+sistema.cadastrarSensor(sensor2)
 
-const leitura1 = new Leitura(sensor1, 120, "20/06/2026", "15:00");
+sistema.registraleituras(
+  new Leitura(sensor1, 120, "20/06/2026", "14:00")
+)
 
-sistema.registraleituras(leitura1);
+sistema.registraleituras(
+  new Leitura(sensor1, 130, "20/06/2026", "14:10")
+)
 
-sistema.listarSensoresAtivos();
-console.log(sistema)
+sistema.registraleituras(
+  new Leitura(sensor1, 140, "20/06/2026", "14:20")
+)
+
+console.log("\n--- Todos os Sensores ---")
+sistema.listarTodosSensores()
+
+console.log("\n--- Leituras Registradas ---")
+sistema.listarLeituras()
+
+console.log("\n--- Sensores Ativos ---")
+sistema.listarSensoresAtivos()
+
+console.log("\n--- Sensores em Manutenção ---")
+sistema.listarSensoresManutencao()
